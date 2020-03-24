0145c1113adef5b68e90868d5fb8719a70f631e7
Norberto Santiago <norbertosantiago82@gmail.com>
2020-03-24 02:14:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -40,7 +40,7 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(name) FROM character_books
-  JOIN characters ON characters.id = character_books.id
+  JOIN characters ON characters.id = character_books.character_id
   GROUP BY characters.name
   ORDER BY COUNT(name) DESC;"
 end
